d3a49810c1eb1b9771320c476301ce192bf45e55
Rick South <rick@natamus.com>
2024-04-28 16:12:45 +0200

    Beautified Chat Client source code for 1.20.5.

---
+++ NeoForge/src/main/java/com/natamus/beautifiedchatclient/neoforge/events/NeoForgeBeautifulChatEvent.java
@@ -0,0 +1,23 @@
+package com.natamus.beautifiedchatclient.neoforge.events;
+
+import com.natamus.beautifiedchatclient.events.BeautifulChatEvent;
+import net.minecraft.network.chat.Component;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientChatReceivedEvent;
+
+@EventBusSubscriber
+public class NeoForgeBeautifulChatEvent {
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent
+	public static void onClientChat(ClientChatReceivedEvent e) {
+		Component originalMessage = e.getMessage();
+		Component newMessage = BeautifulChatEvent.onClientChat(e.getBoundChatType().chatType().value(), e.getMessage(), e.getSender());
+		
+		if (originalMessage != newMessage) {
+			e.setMessage(newMessage);
+		}
+	}
+}
