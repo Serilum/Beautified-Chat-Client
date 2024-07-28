529b9a88ad9b9a8bfb81e01864c8db8c36a23dfa
Rick South <rick@natamus.com>
2024-07-28 07:43:10 +0200

    (v2.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/beautifiedchatclient/ModFabricClient.java
+++ Fabric/src/main/java/com/natamus/beautifiedchatclient/ModFabricClient.java
@@ -3,6 +3,8 @@ package com.natamus.beautifiedchatclient;
 import com.natamus.beautifiedchatclient.events.BeautifulChatEvent;
 import com.natamus.collective.fabric.callbacks.CollectiveChatEvents;
 import net.fabricmc.api.ClientModInitializer;
+import com.natamus.beautifiedchatclient.util.Reference;
+import com.natamus.collective.check.ShouldLoadCheck;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
 
@@ -11,6 +13,10 @@ import java.util.UUID;
 public class ModFabricClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() { 
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		registerEvents();
 	}
 	
